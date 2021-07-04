35279f7c3cc0759904e7402cd75ae7dc36a347a6
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-07-04 16:50:07 +0100

    commit tree 81f3883c34f8097ba7091659e7413a2d3b406857

---
+++ build/precache-manifest.158014ba862144f39dde2e71ec8d261a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "956c7be2dc393907478b",
+    "url": "/static/css/main.b0f594ea.chunk.css"
+  },
+  {
+    "revision": "956c7be2dc393907478b",
+    "url": "/static/js/main.d8aef0b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "6b40d9edd312a718902be000fb7ede6b",
+    "url": "/index.html"
+  }
+];
